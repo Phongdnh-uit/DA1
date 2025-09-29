0f0f13109b4e1ca4635e335cdfa8d33ac482ce76
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-09-29 21:44:51 +0700

    🌟 feat(server): exception handler for apiException

--- backend/src/main/java/com/phongdnh/se121/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/phongdnh/se121/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,20 @@
 package com.phongdnh.se121.exceptions;
 
+import com.phongdnh.se121.dtos.ApiResponse;
+import com.phongdnh.se121.exceptions.errors.ApiException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    
+  @ExceptionHandler(ApiException.class)
+  public ResponseEntity<ApiResponse<Void>> handleApiException(ApiException ex) {
+    ApiResponse<Void> response = new ApiResponse<>();
+    response.setCode(ex.getErrorCode().getCode());
+    response.setMessage(ex.getMessage() != null ? ex.getMessage() : ex.getErrorCode().getMessage());
+    response.setErrors(ex.getFieldErrors());
+    return ResponseEntity.status(ex.getErrorCode().getHttpCode()).body(response);
+  }
 }
