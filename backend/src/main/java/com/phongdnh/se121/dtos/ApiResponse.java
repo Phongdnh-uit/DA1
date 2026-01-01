246450fa3806c4f3d7f4c7d1cf5a5d72999609d9
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-01-01 13:02:27 +0700

    🐛 fix(server): fix log in exception handler

--- backend/src/main/java/com/phongdnh/se121/dtos/ApiResponse.java
+++ backend/src/main/java/com/phongdnh/se121/dtos/ApiResponse.java
@@ -4,9 +4,11 @@ import java.time.Instant;
 import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ApiResponse<T> {
   private Integer code;
   private String message;
